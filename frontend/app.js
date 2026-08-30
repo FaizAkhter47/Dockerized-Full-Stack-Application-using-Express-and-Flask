e094d005ed1b1572f314025529a606ffe12a3269
faizakhter47 <faizakhter47@gmail.com>
2026-08-31 02:56:31 +0530

    loaded

--- frontend/app.js
+++ frontend/app.js
@@ -486,6 +486,10 @@ app.post("/submit", async (req, res) => {
   }
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(3000, () => {
   console.log("Frontend running on port 3000");
 });
